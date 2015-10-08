2f7c66f66c55cfdc1e69bde73df47516d069c2d1
Alvin Lin <alvin.lin@stuypulse.com>
2015-10-08 11:13:42 -0400

    more test files

---
+++ c/sizes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("Size of short: %s\n", sizeof(short));
+  printf("Size of int: %s\n", sizeof(int));
+  printf("Size of float: %s\n", sizeof(float));
+  printf("Size of long: %s\n", sizeof(long));
+  printf("Size of double: %s\n", sizeof(double));
+  printf("Size of char: %s\n", sizeof(char));
+  
+  return 0;
+}
