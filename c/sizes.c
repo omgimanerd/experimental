495328de601f4c83d38a4e6e530f540119d4e382
Alvin Lin <alvin.lin@stuypulse.com>
2015-10-08 11:19:18 -0400

    update

--- c/sizes.c
+++ c/sizes.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  printf("Size of short: %s\n", sizeof(short));
-  printf("Size of int: %s\n", sizeof(int));
-  printf("Size of float: %s\n", sizeof(float));
-  printf("Size of long: %s\n", sizeof(long));
-  printf("Size of double: %s\n", sizeof(double));
-  printf("Size of char: %s\n", sizeof(char));
+  printf("Size of short: %lu bytes\n", sizeof(short));
+  printf("Size of int: %lu bytes\n", sizeof(int));
+  printf("Size of float: %lu bytes\n", sizeof(float));
+  printf("Size of long: %lu bytes\n", sizeof(long));
+  printf("Size of double: %lu bytes\n", sizeof(double));
+  printf("Size of char: %lu bytes\n", sizeof(char));
   
   return 0;
 }
