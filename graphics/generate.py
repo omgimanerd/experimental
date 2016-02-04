ed96334a9768a903400d31febe436c599eacebd3
Alvin Lin <alvin.lin@stuypulse.com>
2016-02-04 13:19:36 -0500

    some line generation stuff

--- graphics/generate.py
+++ graphics/generate.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # Author: Alvin Lin (alvin.lin.dev@gmail.com)
 
+import math
+
 from picture import Picture
 
 def change(current, dimens, color):
@@ -8,9 +10,17 @@ def change(current, dimens, color):
     return [100, 100, 100]
   return color
 
+def is_on_line(a, b, c, px, py):
+  return (abs(px * a + py * b + c) / math.sqrt(a * a + b * b)) < 5
+
+def line(current, dimens, color):
+  if is_on_line(5, -2, -3, current[0], current[1]):
+    return [128, 0, 0]
+  return color
+
 def main():
   p = Picture("test.ppm", 500, 500)
-  p.map(change)
+  p.map(line)
   p.generate()
 
 if __name__ == "__main__":
