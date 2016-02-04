dd5e6298809efd436a6c0699cc853795a3c7ac5b
Alvin Lin <alvin.lin@stuypulse.com>
2016-02-04 12:50:29 -0500

    finished and documented Picture class

--- graphics/generate.py
+++ graphics/generate.py
@@ -3,8 +3,14 @@
 
 from picture import Picture
 
+def change(current, dimens, color):
+  if current[0] > dimens[0] / 2:
+    return [100, 100, 100]
+  return color
+
 def main():
-  p = Picture("test.ppm", 1000, 1000)
+  p = Picture("test.ppm", 500, 500)
+  p.map(change)
   p.generate()
 
 if __name__ == "__main__":
