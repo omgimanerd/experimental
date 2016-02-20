6001361f0fe64fdd69e53b621157e42adb421752
Alvin Lin <alvin.lin.dev@gmail.com>
2016-02-20 18:27:44 -0500

    more deps

--- script/setup.sh
+++ script/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Install Git, Java, Chromium, Emacs, python-dev, and libssl-dev
-sudo apt-get install git default-jdk default-jre chromium-browser emacs24 python-dev libssl-dev
+# Install Git, Java, Chromium, Emacs, OpenSSH, python-dev, and libssl-dev
+sudo apt-get install git default-jdk default-jre chromium-browser emacs24 openssh-server python-dev libssl-dev
 
 # Install Node Version Manager
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
@@ -10,3 +10,8 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | b
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get install -y mongodb-org
+
+# Install f.lux
+sudo add-apt-repository ppa:kilian/f.lux
+sudo apt-get update
+sudo apt-get install fluxgui
