eda5f317a410deb1afbd5ab3295f0974ad4ec8d9
Alvin Lin <alvin.lin.dev@gmail.com>
2016-02-20 17:12:18 -0500

    setup.s

--- script/setup.sh
+++ script/setup.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-sudo apt-get install git default-jdk default-jre chromium-browser emacs24
+# Install Git, Java, Chromium, Emacs, python-dev, and libssl-dev
+sudo apt-get install git default-jdk default-jre chromium-browser emacs24 python-dev libssl-dev
+
+# Install Node Version Manager
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+# Install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get install -y mongodb-org
