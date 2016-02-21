c927b032089d6818546e105f6916d3e8b3b80ad3
Alvin Lin <alvin.lin.dev@gmail.com>
2016-02-21 15:14:48 -0500

    gulpfile update

--- useful_js/gulpfile.js
+++ useful_js/gulpfile.js
@@ -1,58 +1,59 @@
 /**
- * Javascript Task Runner. Depends on
+ * Javascript Task Runner, gulp should be installed globally on the system.
  * @author Alvin Lin (alvin.lin.dev@gmail.com)
  */
 
-/**
- * Dependencies.
- * gulp should be installed globally on the system, any gulp subpackages can
- * be installed locally to the project.
- */
-var gulp = require("gulp");
+// Dependencies
+var gulp = require('gulp');
 
-var autoprefixer = require("gulp-autoprefixer");
-var concat = require("gulp-concat");
-var cssnano = require("gulp-cssnano");
-var less = require("gulp-less");
-var rename = require("gulp-rename");
-var uglify = require("gulp-uglify");
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var closure = require('gulp-closure-compiler-service');
+var cssnano = require('gulp-cssnano');
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
 
-gulp.task("default", ["js", "less"]);
+gulp.task('default', ['js', 'less']);
 
-gulp.task("js", function() {
-  return gulp.src(["./shared/*.js",
-                   "./static/js/game/*.js",
-                   "./static/js/*.js"])
-    .pipe(concat("minified.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest("static/dist"));
+gulp.task('js', function() {
+  return gulp.src(['./shared/*.js',
+                   './static/js/game/*.js',
+                   './static/js/*.js'])
+    .pipe(plumber())
+    .pipe(concat('minified.js'))
+    .pipe(closure({
+      compilation_level: 'SIMPLE_OPTIMIZATIONS'
+    }))
+    .pipe(gulp.dest('static/dist'));
 });
 
-gulp.task("less", function() {
-  return gulp.src("./static/less/styles.less")
+gulp.task('less', function() {
+  return gulp.src('./static/less/styles.less')
+    .pipe(plumber())
     .pipe(less({ compress: true}))
     .pipe(autoprefixer())
     .pipe(cssnano())
     .pipe(rename(function(path) {
-      path.basename = "minified";
-      path.extname = ".css";
+      path.basename = 'minified';
+      path.extname = '.css';
     }))
-    .pipe(gulp.dest("./static/dist"));
+    .pipe(gulp.dest('./static/dist'));
 });
 
-gulp.task("watch-js", function() {
- gulp.watch(["./shared/*.js",
-             "./static/js/*.js",
-             "./static/js/game/*.js"], ["js"]);
+gulp.task('watch-js', function() {
+  gulp.watch(['./shared/*.js',
+             './static/js/*.js',
+             './static/js/game/*.js'], ['js']);
 });
 
-gulp.task("watch-less", function() {
-  gulp.watch("./static/less/*.less", ["less"]);
+gulp.task('watch-less', function() {
+  gulp.watch('./static/less/*.less', ['less']);
 });
 
-gulp.task("watch", function() {
- gulp.watch(["./shared/*.js",
-             "./static/js/*.js",
-             "./static/js/game/*.js"], ["js"]);
-  gulp.watch("./static/less/*.less", ["less"]);
+gulp.task('watch', function() {
+ gulp.watch(['./shared/*.js',
+             './static/js/*.js',
+             './static/js/game/*.js'], ['js']);
+  gulp.watch('./static/less/*.less', ['less']);
 });
