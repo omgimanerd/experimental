d2c3402815ba2907893ef93c32b606f47f9a644a
omgimanerd <alvin.lin.dev@gmail.com>
2016-03-02 18:13:00 -0500

    updated generate.py

---
+++ gov_hw/README.md
@@ -0,0 +1,14 @@
+# US Government Class Homework
+
+Using pdf2txt.py, we dump the contents of the textbook into a text file, stored
+in /raw. We then parse the text file to extract a meaningful summary of each
+chapter.
+
+## Dependencies
+```
+sudo pip install --upgrade 3to2           # Runs on Python 2.7
+sudo pip install --upgrade language-check
+```
+
+## Creators
+Alvin Lin
--- gov_hw/generate.py
+++ gov_hw/generate.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python
 
+import argparse
 import language_check
 import random
 import re
 
-if __name__ == '__main__':
-  checker = language_check.LanguageTool('en-US')
-
-  with open("ch01.txt") as f:
+def main():
+  argument_parser = argparse.ArgumentParser(description='Does gov homework.')
+  argument_parser.add_argument('filename',
+                               help='The file containing the chapter text dump')
+  argument_parser.add_argument('section', type=int,
+                               help='The section to generate a summary for')
+  args = argument_parser.parse_args()
+  
+  # Reads in the chapter
+  with open(args.filename) as f:
     chapter = f.read().strip()
 
-  match_3 = re.search("S e c t i on\s+[4]", chapter)
-  match_3_end = re.search("Section\s+4\sAssessment", chapter)
+  # Isolates a single section of to summarize.
+  section_begin = re.search('S\s*e\s*c\s*t\s*i\s*o\s*n\s*%s' % args.section,
+                            chapter)
+  section_end = re.search('Section\s*%s\s*Assessment' % args.section,
+                          chapter)
+  section = chapter[section_begin.start(): section_end.start()]
 
-  section = chapter[match_3.start():match_3_end.start()]
-  section = re.sub("[^\w\s .'\"!,()-]", '', section)
-  section = section.split("\n")
-  section = map(lambda x: x.strip(), section)
-  section = filter(lambda x: x != '', section)
+  # Removes all extraneous whitespace and symbols and separates the section
+  # into a list of sentences.
+  section = re.sub("[^\w\s .'\"!,()]", '', section)
+  section = section.replace('. . .', '')
+  section = re.sub('\s+', ' ', section)
+  section = filter(lambda x: x != '', section.split('. '))
+  print section
+
+  # Filter out pertinent and useful sentences
+
+  # Rejoin the list of sentences
   section = ' '.join(section).split('. ')
-  section = filter(lambda x: x != '', section)
+  print section
 
-  # section = filter(lambda x: len(checker.check(x)) == 0, section)
-  
-  print ". ".join(section)
+  # # section = filter(lambda x: len(checker.check(x)) == 0, section)
+
+
+if __name__ == '__main__':
+  main()
