0e8b94cddaa38325f89e50dc33de6778f3c77120
omgimanerd <alvin.lin.dev@gmail.com>
2016-03-10 19:33:30 -0500

    updated setup script

--- script/setup.sh
+++ script/setup.sh
@@ -24,14 +24,17 @@ install_flux () {
     sudo apt-get install fluxgui
 }
 
-# Install Git, Emacs, OpenSSH, python-dev, python-pip, xclip, screen, make, and libssl-dev
-confirm "Install Git, Emacs, python-dev, python-pip, xclip, screen, make, tkdiff, and libssl-dev? [y/n]" && sudo apt-get install git emacs24 python-dev python-pip xclip screen make tkdiff libssl-dev
+# Install Git, Emacs, OpenSSH, python-dev, python-pip, xclip, ngrok, screen, make, and libssl-dev
+confirm "Install Git, Emacs, python-dev, python-pip, xclip, ngrok, screen, make, tkdiff, and libssl-dev? [y/n]" && sudo apt-get install git emacs24 python-dev python-pip xclip ngrok-client screen make tkdiff libssl-dev
 
 # Install Chromium
 confirm "Install Chromium? [y/n]" && sudo apt-get install chromium-browser
 
 # Install Gimp
 confirm "Install Gimp? [y/n]" && sudo apt-get install gimp
+
+# Install Heroku
+confirm "Install Heroku? [y\n]" && wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Install Java
 confirm "Install Java JRE and JDK? [y/n]" && sudo apt-get install default-jre default-jdk
