c237a1c22398acc03621316b13de8eb52e933580
bleepbloopsify <bleepbloopsify@gmail.com>
2016-04-07 08:51:04 -0400

    pls

--- gov_hw/generate.sh
+++ gov_hw/generate.sh
@@ -4,5 +4,5 @@ read -p Chapter: chapter
 
 for i in `seq 1 4`;
 do
-	python generate.py "./raw/ch0$chapter.txt" $i > "./summary/$chapter\_$i.txt"
+	python generate.py "./raw/ch0$chapter.txt" $i > "./summary/"$chapter"_$i.txt"
 done
