2cdbb85027bbd17d7dab2a3bdf2c4d22fbe72f8d
omgimanerd <alvin.lin.dev@gmail.com>
2016-09-26 13:06:42 -0400

    calc.py

--- calc.py
+++ calc.py
@@ -6,23 +6,26 @@ def delta_x(a, b, n):
     return (b - a) / n
 
 def left_riemann_sum(f, a, b, n):
-    return delta_x(a, b, n) * sum(
-        [f(x) for x in linspace(a, b, n + 1, endpoint=False)])
+    interval = linspace(a, b, n + 1, endpoint=False)
+    return (interval, delta_x(a, b, n) * sum([f(x) for x in interval]))
 
 def right_riemann_sum(f, a, b, n):
-    return delta_x(a, b, n) * sum(
-        [f(x) for x in linspace(a, b, n + 1)[1:]])
+    interval = linspace(a, b, n + 1)[1:]
+    return (interval, delta_x(a, b, n) * sum([f(x) for x in interval]))
 
 def midpoint_approximation(f, a, b, n):
     delta = delta_x(a, b, n)
-    return delta * sum(
-        [f(x) for x in arange(a + (delta / 2), b, delta)])
+    interval = arange(a + (delta / 2), b, delta)
+    return (interval, delta * sum([f(x) for x in interval]))
 
 def trapezoidal_approximation(f, a, b, n):
-    return (left_riemann_sum(f, a, b, n) + right_riemann_sum(f, a, b, n)) / 2
+    interval = linspace(a, b, n + 1)
+    return (interval,
+            (left_riemann_sum(f, a, b, n)[1] + right_riemann_sum(f, a, b, n)[1]) / 2)
 
 def simpsons_rule_approximation(f, a, b, n):
     series = linspace(a, b, n + 1)
+    interval = series
     for index, term in enumerate(series):
         if index == 0 or index == len(series) - 1:
             series[index] = f(term)
@@ -30,7 +33,7 @@ def simpsons_rule_approximation(f, a, b, n):
             series[index] = 2 * f(term)
         else:
             series[index] = 4 * f(term)
-    return (delta_x(a, b, n) / 3) * sum(series)
+    return (interval, (delta_x(a, b, n) / 3) * sum(series))
 
 if __name__ == '__main__':
     from math import *
@@ -39,13 +42,13 @@ if __name__ == '__main__':
     b = int(input("Upper limit: "))
     n = int(input("n: "))
     print("-----")
-    print("Left Riemann Sum Approximation: {}".format(
-        left_riemann_sum(f, a, b, n)))
-    print("Right Riemann Sum Approximation: {}".format(
-        right_riemann_sum(f, a, b, n)))
-    print("Midpoint Sum Approximation: {}".format(
-        midpoint_approximation(f, a, b, n)))
-    print("Trapezoidal Sum Approximation: {}".format(
-        trapezoidal_approximation(f, a, b, n)))
-    print("Simpson's Rule Approximation: {}".format(
-        simpsons_rule_approximation(f, a, b, n)))
+    print("Interval: {0}\nLeft Riemann Sum Approximation: {1}\n".format(
+        *left_riemann_sum(f, a, b, n)))
+    print("Interval: {0}\nRight Riemann Sum Approximation: {1}\n".format(
+        *right_riemann_sum(f, a, b, n)))
+    print("Interval: {0}\nMidpoint Sum Approximation: {1}\n".format(
+        *midpoint_approximation(f, a, b, n)))
+    print("Interval: {0}\nTrapezoidal Sum Approximation: {1}\n".format(
+        *trapezoidal_approximation(f, a, b, n)))
+    print("Interval: {0}\nSimpson's Rule Approximation: {1}\n".format(
+        *simpsons_rule_approximation(f, a, b, n)))
