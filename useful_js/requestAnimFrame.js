aad944a398f6a9ace0a0902a9a4bfd08ae40765c
Alvin Lin <omgimanerd@google.com>
2017-06-13 12:13:37 -0700

    ?

--- useful_js/requestAnimFrame.js
+++ useful_js/requestAnimFrame.js
@@ -3,13 +3,14 @@
  * @author alvin.lin.dev@gmail.com (Alvin Lin)
  */
 
-window.requestAnimFrame = (function() {
+var requestAnimationFrame = (function() {
+  var timeoutFn = function(callback, element) {
+    window.setTimeout(callback, 1000 / 60);
+  };
   return window.requestAnimationFrame ||
-         window.webkitRequestAnimationFrame ||
-         window.mozRequestAnimationFrame ||
-         window.oRequestAnimationFrame ||
-         window.msRequestAnimationFrame ||
-         function(callback, element) {
-           window.setTimeout(callback, 1000 / 60);
-         };
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    timeoutFn;
 })();
