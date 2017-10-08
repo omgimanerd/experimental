73fb11944de4e0f4397587d8fa2d93c02396b949
omgimanerd <alvin@omgimanerd.tech>
2017-10-08 01:16:23 -0400

    more jalvin code

--- jalvin/electric_longboard/electric_longboard.ino
+++ jalvin/electric_longboard/electric_longboard.ino
@@ -1,42 +1,83 @@
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+/**
+ * Code powering our electric longboard. For reference, the servo angle maps to
+ * a power for the brushless motor. During calibration and run, 0 - 90 is used
+ * to calibrate and afterwards, 90 represents no power, and 180 represents full
+ * power.
+ */
 
-RF24 radio(9, 10);
+#include <Servo.h>
 
-const byte rxAddr[6] = "00001";
+#define MOTOR_PIN 9
+#define POTENTIOMETER_PIN A0
+#define BUTTON_PIN A1
+
+// Units per millisecond
+#define MIN_POWER 90
+#define MAX_POWER 180
+#define POWER_DECAY_RATE 0.025
+
+// Initialize the driving motor
+Servo motor;
+
+// State variables
+float currentPower = MIN_POWER;
+long lastUpdateTime = 0;
+float deltaTime = 0;
+
+void stepPower(int start_power, int end_power, int delay_ms) {
+  if (start_power < end_power) {
+    for (int i = start_power; i <= end_power; ++i) {
+      motor.write(i);
+      delay(delay_ms);
+    }
+  } else {
+    for (int i = start_power; i >= end_power; --i) {
+      motor.write(i);
+      delay(delay_ms);
+    }
+  }
+}
+
+void initialize() {
+  // This initial delay allows everything to start up before we begin
+  // calibration
+  delay(2000);
+  stepPower(90, 0, 2);
+  delay(200);
+  stepPower(0, 90, 2);
+  delay(4500);
+  stepPower(90, 0, 2);
+}
 
 void setup() {
-  radio.begin();
-  radio.setRetries(15, 15);
-  radio.openWritingPipe(rxAddr);
-  
-  radio.stopListening();
+  Serial.begin(9600);
+
+  motor.attach(MOTOR_PIN);
+  initialize();
+
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, HIGH);
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  
-  delay(1000);
+  int throttle = analogRead(POTENTIOMETER_PIN);
+  throttle = map(throttle, 0, 1023, MIN_POWER, MAX_POWER);
+  int button = digitalRead(BUTTON_PIN);
+
+  long currentTime = micros();
+  deltaTime = (currentTime - lastUpdateTime) / 1000;
+  lastUpdateTime = currentTime;
+
+  /**
+   * If the button is pressed, we set that to the throttle, otherwise we
+   * start scaling down the power (coasting).
+   */
+  if (!button) {
+    currentPower = throttle;
+  } else {
+    currentPower = max(MIN_POWER, currentPower - (deltaTime * POWER_DECAY_RATE));
+  }
+  Serial.println(currentPower);
+
+  motor.write((int) currentPower);
 }
-
-//#include <Servo.h>
-//
-//#define SERVO_PIN 9
-//#define POT A0
-//
-//Servo esc;
-// 
-//void setup() {
-//  esc.attach(SERVO_PIN);
-//  Serial.begin(9600);
-//}
-//
-//void loop() {
-//  int throttle = analogRead(POT);
-//  throttle = map(throttle, 0, 1023, 0, 179);
-//  Serial.println(throttle);
-//  esc.write(throttle);
-//}
-
--- jalvin/rfid_door_lock/rfid_door_lock.ino
+++ jalvin/rfid_door_lock/rfid_door_lock.ino
@@ -1,73 +1,79 @@
 /*
- * MFRC522 - Library to use ARDUINO RFID MODULE KIT 13.56 MHZ WITH TAGS SPI W AND R BY COOQROBOT.
- * The library file MFRC522.h has a wealth of useful info. Please read it.
- * The functions are documented in MFRC522.cpp.
- *
- * Based on code Dr.Leong   ( WWW.B2CQSHOP.COM )
- * Created by Miguel Balboa (circuitito.com), Jan, 2012.
- * Rewritten by Søren Thing Andersen (access.thing.dk), fall of 2013 (Translation to English, refactored, comments, anti collision, cascade levels.)
- * Released into the public domain.
- *
- * Sample program showing how to read data from a PICC using a MFRC522 reader on the Arduino SPI interface.
- *----------------------------------------------------------------------------- empty_skull 
- * Aggiunti pin per arduino Mega
- * add pin configuration for arduino mega
- * http://mac86project.altervista.org/
- ----------------------------------------------------------------------------- Nicola Coppola
- * Pin layout should be as follows:
- * Signal     Pin              Pin               Pin
- *            Arduino Uno      Arduino Mega      MFRC522 board
- * ------------------------------------------------------------
- * Reset      9                5                 RST
- * SPI SS     10               53                SDA
- * SPI MOSI   11               51                MOSI
- * SPI MISO   12               50                MISO
- * SPI SCK    13               52                SCK
- *
- * The reader can be found on eBay for around 5 dollars. Search for "mf-rc522" on ebay.com. 
+ * Controller for James's door.
+ * Author: Alvin Lin (alvin@omgimanerd.tech)
  */
 
-#include <Servo.h>
-#include <SPI.h>
 #include <MFRC522.h>
+#include <SPI.h>
 
-#define UID_LENGTH 10
-#define SS_PIN 10
 #define RST_PIN 9
+#define SS_PIN 10
+#define NUM_ACCESS_CODES 2
+#define CODE_LENGTH 10
+
+#define STEP_GROUND_PIN 3
+#define STEP_POWER_PIN 4
+#define STEP_DIRECTION_PIN 5
+#define STEPS_PER_REV 200
+
+#define LED_GROUND A4
+#define LED_POWER A5
 
 MFRC522 rfid(SS_PIN, RST_PIN);
-Servo lock;
 
-const byte CORRECT[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+/**
+ * The valid UID access bits.
+ */
+const byte ACCESS[NUM_ACCESS_CODES][CODE_LENGTH] = {
+  { 11, 87, 11, 35, 0, 0, 0, 0, 0, 0 },
+  { 229, 50, 174, 41, 0, 0, 0, 0, 0, 0 }
+};
 
+/**
+ * We'll be hardcoding the correct card UID into the program.
+ * Obviously not secure, but who the fuck is going to hack this?
+ */
 bool authenticate(byte input[]) {
-  for (int i = 0; i < UID_LENGTH; ++i) {
-    if (input[i] != CORRECT[i]) {
-      return false;
+  for (int code = 0; code < NUM_ACCESS_CODES; ++code) {
+    for (int i = 0; i < CODE_LENGTH; ++i) {
+      if (input[i] != ACCESS[code][i]) {
+        break;
+      }
     }
+    return true;
   }
-  return true;
+  return false;
 }
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communications with the PC
-  SPI.begin();      // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 card
-  pinMode(5, OUTPUT);
-  lock.attach(6);
+  Serial.begin(9600);
+  rfid.PCD_Init(); // Initialize RFID reader
+
+  pinMode(STEP_GROUND_PIN, OUTPUT);
+  pinMode(STEP_POWER_PIN, OUTPUT);
+  pinMode(STEP_DIRECTION_PIN, OUTPUT);
+  digitalWrite(STEP_GROUND_PIN, LOW);
+  digitalWrite(STEP_POWER_PIN, LOW);
+  digitalWrite(STEP_DIRECTION_PIN, LOW);
+
+  pinMode(LED_GROUND, OUTPUT);
+  pinMode(LED_POWER, OUTPUT);
+  digitalWrite(LED_GROUND, LOW);
+  digitalWrite(LED_POWER, LOW);
 }
 
 void loop() {
-  lock.write(0);
-  if (rfid.PICC_IsNewCardPresent()) {
-    if (rfid.PICC_ReadCardSerial()) {
-      bool authenticated = authenticate(rfid.uid.uidByte);
-      if (authenticated) {
-        lock.write(180);
-        delay(1000);
-      }
-    }
-  }
+//  if (rfid.PICC_IsNewCardPresent()) {
+//    if (rfid.PICC_ReadCardSerial()) {
+//      if (authenticate(rfid.uid.uidByte)) {
+//        delay(1000);
+//      }
+//    }
+//  }
+  digitalWrite(STEP_POWER_PIN, HIGH);
+  delay(5);
+  digitalWrite(STEP_POWER_PIN, LOW);
+  delay(5);
 }
 
 
