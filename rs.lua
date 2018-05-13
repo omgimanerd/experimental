642bed3a74cf299a53b398544f5b6642160676a3
omgimanerd <alvin.lin.dev@gmail.com>
2018-05-12 22:15:26 -0400

    more changes

--- rs.lua
+++ rs.lua
@@ -6,10 +6,16 @@ local component = require('component')
 local sides = require('sides')
 
 local rs = component.block_refinedstorage_interface
+local threshold = 500
+
+local function isempty(s)
+  return s == nil or s == ""
+end
 
 for i, item in ipairs(rs.getItems()) do
-  print(i, item)
-  for j, k in ipairs(item) do
-    print(j, k)
+  if item.size < threshold then
+    if !isempty(string.find(item.name, "biomesoplenty")) then
+      print("Marked " .. item.label .. " for restocking")
+    end
   end
 end
