7653484f41bb89ff4fddef6924822b014960344d
omgimanerd <alvin@omgimanerd.tech>
2018-12-09 22:11:35 -0500

    scary shit

--- canvas_fingerprinting/client/index.js
+++ canvas_fingerprinting/client/index.js
@@ -3,12 +3,63 @@
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
 
-const el = document.getElementById
+const socket = io()
 
-const getCanvasFingerprint = () => {
+const TEXT = 'The quick brown fox jumped over the lazy dog'
 
+const WIDTH = 600
+const HEIGHT = 30
+
+const elid = id => document.getElementById(id)
+const el = name => document.createElement(name)
+
+const getCanvasFingerprint = font => {
+  const canvas = elid('canvas')
+  canvas.width = WIDTH
+  canvas.height = HEIGHT
+  const context = canvas.getContext('2d')
+  context.font = font
+  context.textBaseline = 'top'
+  context.fillText(TEXT, 0, 0)
+  context.fillStyle = '#ABCDEF'
+  context.fillText(TEXT, 60, 10)
+  const data = context.getImageData(0, 0, WIDTH, HEIGHT).data
+  return new Hashes.MD5().hex(String(data))
 }
 
-document.onload = () => {
+const renderTableDisplay = (selfFingerprint, fingerprints) => {
+  const table = elid('display')
+  while (table.firstChild) {
+    table.removeChild(table.firstChild)
+  }
 
+  const addTableRow = (t1HTML, t2HTML, current) => {
+    const trEl = el('tr')
+    if (current) {
+      trEl.className = 'current'
+    }
+    const t1El = el('td')
+    t1El.innerHTML = t1HTML
+    const t2El = el('td')
+    t2El.innerHTML = t2HTML
+    trEl.appendChild(t1El)
+    trEl.appendChild(t2El)
+    table.appendChild(trEl)
+  }
+
+  addTableRow('Fingerprint', 'Visits')
+  for (const fingerprint in fingerprints) {
+    addTableRow(fingerprint, fingerprints[fingerprint],
+      selfFingerprint == fingerprint)
+  }
 }
+
+window.addEventListener('load', () => {
+  const hash = getCanvasFingerprint('12pt Arial')
+  elid('fingerprint').innerHTML = `Canvas Fingerprint: ${hash}`
+  socket.emit('fingerprint', hash)
+
+  socket.on('render', fingerprints => {
+    renderTableDisplay(hash, fingerprints)
+  })
+})
--- canvas_fingerprinting/server.js
+++ canvas_fingerprinting/server.js
@@ -6,17 +6,35 @@
 const PORT = 5000
 
 const express = require('express')
+const http = require('http')
 const path = require('path')
 const socket = require('socket.io')
+const useragent = require('express-useragent')
 
 const app = express()
+const server = http.Server(app)
+const io = socket(server)
+
+app.use(express.static('client'))
+app.use(express.static('node_modules'))
+app.use(useragent.express());
 
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.use(express.static('client'))
+/* kv-pairs of hash to hits */
+const uniqueUsers = {}
 
-app.listen(PORT, () => {
+io.on('connection', socket => {
+  socket.on('fingerprint', fingerprint => {
+    uniqueUsers[fingerprint] = uniqueUsers[fingerprint] ?
+      uniqueUsers[fingerprint] + 1 : 1
+
+    io.sockets.emit('render', uniqueUsers)
+  })
+})
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
