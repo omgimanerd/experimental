ee78f09b3f46a68c07e9bdc23c4334bf5dfff914
omgimanerd <alvin@omgimanerd.tech>
2024-10-05 23:15:03 -0400

    Update all dotfiles.

--- dotfiles/.bash_aliases
+++ dotfiles/.bash_aliases
@@ -3,3 +3,5 @@
 alias l='ls'
 alias la='ls -la'
 alias ll='ls -l'
+
+alias python='python3'
--- dotfiles/.ps1
+++ dotfiles/.ps1
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-# Returns the command prompt formatted with the current git workspace state
+# Escapes the non-printing characters in the given argument string. If the
+# command prompt is not escaped, the text length of ANSI color codes is
+# computed incorrectly and the cursor will jump erratically.
+# https://stackoverflow.com/a/52465778/5792737
+#
+# This is the easiest way to use color codes in PS1 because \[ and \] are
+# treated as special characters and cannot be outputted in echo.
+function readline_ANSI_escape() {
+  if [[ $# -ge 1 ]]; then
+    echo "$*"
+  else
+    cat  # Read string from STDIN
+  fi | \
+  perl -pe 's/(?:(?<!\x1)|(?<!\\\[))(\x1b\[[0-9;]*[mG])(?!\x2|\\\])/\x1\1\x2/g'
+}
+
+# Sets and exports PS1 formatted with the current git workspace state
 # if the current working directory is a git directory.
 function ps1_() {
-
-  # Escapes the non-printing characters in the given argument string. If the
-  # command prompt is not escaped, the text length of ANSI color codes is
-  # computed incorrectly and the cursor will jump erratically.
-  # https://stackoverflow.com/a/52465778/5792737
-  function readline_ANSI_escape() {
-    if [[ $# -ge 1 ]]; then
-      echo "$*"
-    else
-      cat  # Read string from STDIN
-    fi | \
-    perl -pe 's/(?:(?<!\x1)|(?<!\\\[))(\x1b\[[0-9;]*[mG])(?!\x2|\\\])/\x1\1\x2/g'
-  }
-
   # ANSI-C Quoted Strings for the ANSI color codes
   # https://www.gnu.org/software/bash/manual/html_node/ANSI_002dC-Quoting.html
   local RESET=$'\e[0m'
@@ -43,16 +45,22 @@ function ps1_() {
   local WHITE=$'\e[97m'
 
   local GIT_STATUS=$(git status -s 2> /dev/null | cut -c1,2 | sort | uniq | \
-    sed -r "s/([A-Z])([A-Z])/${LIGHT_GREEN}\1${RED}\2/" | \
-    sed -r "s/ ([A-Z])/${RED}\1/" | \
-    sed -r "s/([A-Z]) /${LIGHT_GREEN}\1/" | \
+    # When a file is modified, staged, and modified again, it will show up as
+    # AM in the shortened git status. Capture both and color them correctly.
+    sed -r "s/([AM])([AM])/${LIGHT_GREEN}\1${RED}\2/" | \
+    # Files that are modified but not staged show up as M on the right column.
+    sed -r "s/ (M)/${RED}\1/" | \
+    # Files that are staged for commit can be either added new files or modified
+    # existing files.
+    sed -r "s/([AM]) /${LIGHT_GREEN}\1/" | \
+    # Newly added files show up as '?'
     sed "s/??/${RED}?${RESET}/" | \
     paste -sd ' ')
   local GIT_STATUS_PROMPT="${WHITE}[${GIT_STATUS}${WHITE}]${RESET} "
-  local USER_PROMPT="${LIGHT_GREEN}$(whoami)${RESET}:"
-  local DIRS_PROMPT="${LIGHT_BLUE}$(dirs)${RESET}"
+  local USER_="${LIGHT_GREEN}${USER}${RESET}:"
+  local PWD_="${LIGHT_BLUE}${PWD}${RESET}"
 
-  readline_ANSI_escape "$GIT_STATUS_PROMPT${USER_PROMPT}${DIRS_PROMPT}\$ "
+  readline_ANSI_escape "${USER_}${PWD_}\$ "
 }
 
 # ps1 function is single quoted for evaluation in the terminal itself.
